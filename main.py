e0234f799d94c11647dd9b59a3878194319a3b56
Alfie Likes Computers <63899598+AeEn123@users.noreply.github.com>
2024-12-04 22:56:01 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+class alfie:
+    class likes:
+        computers = "computers"
+
+class back:
+    class to:
+        home = "home"
+
+
+
+
+
+
+
+alfie.likes.computers
+
+
+
+
+
+
+back.to.home
+
+
+# Images taken at 200% zoom level in Visual Studio Code
